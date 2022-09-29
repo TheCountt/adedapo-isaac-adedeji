882628ec618c5e6bae6dc717ab64d4a591194e89
TheCountt <isaacadedeji0@yahoo.com>
2022-09-29 22:05:46 +0000

    made changes to names

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -6,7 +6,7 @@
 #
 
 resource "aws_iam_role" "demo-cluster" {
-  name = "terraform-eks-demo-cluster"
+  name = "parallelscore-eks-cluster"
 
   assume_role_policy = <<POLICY
 {
@@ -35,7 +35,7 @@ resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSVPCResourceCont
 }
 
 resource "aws_security_group" "demo-cluster" {
-  name        = "terraform-eks-demo-cluster"
+  name        = "parallelscore-eks-cluster"
   description = "Cluster communication with worker nodes"
   vpc_id      = aws_vpc.demo.id
 
--- eks-worker-nodes.tf
+++ eks-worker-nodes.tf
@@ -44,6 +44,7 @@ resource "aws_eks_node_group" "demo" {
   node_role_arn   = aws_iam_role.demo-node.arn
   subnet_ids      = aws_subnet.demo[*].id
   instance_types  = ["t3.medium","t3.micro"]
+  
 
   scaling_config {
     desired_size = 3
